1c8cd11d52e47d20d164a4282639c3ee2de84833
aliaagheisX <aliaagheis@gmail.com>
2026-04-19 07:36:16 +0200

    fix: agentoutput initalize with nones

--- src/agents/base.py
+++ src/agents/base.py
@@ -4,10 +4,10 @@ from PIL.Image import Image
 
 
 class AgentOutput(BaseModel):
-    coordinate: tuple[float, float] | None = Field(..., description="(x,y) normalized, for click or grounding")
-    action_type: str | None = Field(..., description="action type to parse it - later for MMM-Benchmarks or OS-World")
-    text: str | None = Field(..., description="if there's text need to written")
-    raw: dict | None = Field(..., description="raw output from agent - for debugging")
+    coordinate: tuple[float, float] | None = Field(default=None, description="(x,y) normalized, for click or grounding")
+    action_type: str | None = Field(default=None, description="action type to parse it - later for MMM-Benchmarks or OS-World")
+    text: str | None = Field(default=None, description="if there's text need to written")
+    raw: dict | None = Field(default=None, description="raw output from agent - for debugging")
 
 class GUIAgent(ABC):
     def __init__(self, config: dict[str, any]) -> None:
